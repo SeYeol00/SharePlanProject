4e09f5351a71312d04c6c4cf5c1d763064dbc3da
WE-DA-pluggg <79959576+WE-DA-pluggg@users.noreply.github.com>
2021-11-08 18:34:42 +0900

    유저 클래스와 전체 강의 클래스 추가

---
+++ app/src/main/java/com/example/shareplan/UserInfo.java
@@ -0,0 +1,49 @@
+package com.example.shareplan;
+
+public class UserInfo {
+    String Email; // 이메일
+    String Id; // 아이디
+    String Password; // 비밀번호
+    String Name; // 이름
+    Boolean Authority; // 강의 생성 권한
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Boolean getAuthority() {
+        return Authority;
+    }
+
+    public void setAuthority(Boolean authority) {
+        Authority = authority;
+    }
+}
