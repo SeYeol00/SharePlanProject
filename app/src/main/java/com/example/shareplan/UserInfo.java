83fe9853906d6c6eb34038211ac24f47350a4040
WE-DA-pluggg <79959576+WE-DA-pluggg@users.noreply.github.com>
2021-11-12 14:45:15 +0900

    11/12 클래스리스트 Xml 조정

--- app/src/main/java/com/example/shareplan/UserInfo.java
+++ app/src/main/java/com/example/shareplan/UserInfo.java
@@ -5,7 +5,7 @@ public class UserInfo {
     String Id; // 아이디
     String Password; // 비밀번호
     String Name; // 이름
-    Boolean Authority;
+    Boolean Authority;// 강의 생성 권한
 
     public UserInfo() { }
 
@@ -17,7 +17,7 @@ public class UserInfo {
         PhoneNumber = phoneNumber;
     }
 
-    String PhoneNumber;// 강의 생성 권한
+    String PhoneNumber;
 
     public String getEmail() {
         return Email;
