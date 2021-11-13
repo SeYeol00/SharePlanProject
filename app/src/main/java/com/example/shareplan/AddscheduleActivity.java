7aa230aca5b01ce4276ae5c5a39b2398703acf96
choieunsol <achldmsthf@kookmin.ac.kr>
2021-11-13 14:09:04 +0900

    일정 추가 xml 수정, schedule xml 수정

--- app/src/main/java/com/example/shareplan/AddscheduleActivity.java
+++ app/src/main/java/com/example/shareplan/AddscheduleActivity.java
@@ -1,8 +1,16 @@
 package com.example.shareplan;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddscheduleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_schedule);
+
+
+    }
 }
