e599a0bcf8c23ce35d49e8e30ead137eed6900f9
황재일 <chris0765@kookmin.ac.kr>
2021-11-17 01:26:29 +0900

    Modifying CreateLec_2_Acitvity to use ListView to show StuNums

--- app/src/main/java/com/example/shareplan/CreateLec_2_Activity.java
+++ app/src/main/java/com/example/shareplan/CreateLec_2_Activity.java
@@ -5,11 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -33,17 +42,54 @@ public class CreateLec_2_Activity extends AppCompatActivity {
 
         mDatabaseRef = FirebaseDatabase.getInstance().getReference("SharePlan");
 
-        Intent intent = getIntent();
+        Intent newIntent = getIntent();
         EditText studentsText = findViewById(R.id.studentNumber);
+
+        ListView listView = (ListView) findViewById(R.id.nums_listView);
+
+        CreateLec_2_Activity.ListViewAdapter adapter = new CreateLec_2_Activity.ListViewAdapter(CreateLec_2_Activity.this);
+        listView.setAdapter(adapter);
+
+        Button add = findViewById(R.id.addStudent);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stuNum = studentsText.getText().toString();
+                mDatabaseRef.child("UserInfo").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        boolean isExist = false;
+                        for(DataSnapshot userData : snapshot.getChildren()) {
+                            UserInfo userInfo = userData.getValue(UserInfo.class);
+                            if(userInfo.getStunum().equals(stuNum)) {
+                                adapter.addItem(userInfo);
+                                isExist = true;
+                                break;
+                            }
+                        }
+                        if(isExist == false) {
+                            Toast.makeText(getApplicationContext(), "존재하지 않는 학생입니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
 
         Button register = findViewById(R.id.lectureRegister);
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String[] studentsNumber = studentsText.getText().toString().split("\n");
+                ArrayList<UserInfo> updateNumbers = new ArrayList<>();
 
-                ArrayList<String> updateNumbers = new ArrayList<>(Arrays.asList(studentsNumber));
+                for(int i=0; i<adapter.getCount(); i++) {
+                    updateNumbers.add(adapter.getItem(i));
+                }
 
                 mDatabaseRef.child("LectureInfo").addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
@@ -55,34 +101,17 @@ public class CreateLec_2_Activity extends AppCompatActivity {
                         }
 
                         LectureInfo newLecture = new LectureInfo();
-                        newLecture.setName(intent.getStringExtra("Name"));
-                        newLecture.setProfessor(intent.getStringExtra("Professor"));
-                        newLecture.setDay(intent.getStringExtra("Day"));
-                        newLecture.setDivision(intent.getStringExtra("Division"));
-                        newLecture.setTime(intent.getStringExtra("Time"));
+                        newLecture.setName(newIntent.getStringExtra("Name"));
+                        newLecture.setProfessor(newIntent.getStringExtra("Professor"));
+                        newLecture.setDay(newIntent.getStringExtra("Day"));
+                        newLecture.setDivision(newIntent.getStringExtra("Division"));
+                        newLecture.setTime(newIntent.getStringExtra("Time"));
                         lectures.add(newLecture);
 
                         mDatabaseRef.child("LectureInfo").setValue(lectures);
 
-                        for(String num : updateNumbers) {
-                            mDatabaseRef.child("UserInfo").addListenerForSingleValueEvent(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                    for(DataSnapshot userData : snapshot.getChildren()) {
-                                        UserInfo userInfo = userData.getValue(UserInfo.class);
-                                        if(userInfo.getStunum().equals(num)) {
-                                            String studentName = userInfo.getName();
-                                            mDatabaseRef.child("ClassInfo").child(String.valueOf(lectures.size() - 1)).child(num).setValue(studentName);
-                                            break;
-                                        }
-                                    }
-                                }
-
-                                @Override
-                                public void onCancelled(@NonNull DatabaseError error) {
-
-                                }
-                            });
+                        for(UserInfo num : updateNumbers) {
+                            mDatabaseRef.child("ClassInfo").child(String.valueOf(lectures.size() - 1)).child(num.getStunum()).setValue(num.getName());
                         }
                     }
 
@@ -96,6 +125,79 @@ public class CreateLec_2_Activity extends AppCompatActivity {
                 finish();
             }
         });
+    }
 
+    class StudentNumberItemView extends LinearLayout {
+        TextView name;
+        TextView number;
+
+        public StudentNumberItemView(Context context) {
+            super(context);
+            init(context);
+        }
+
+        public StudentNumberItemView(Context context, AttributeSet attrs) {
+            super(context, attrs);
+            init(context);
+        }
+
+        public void init(Context context) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            inflater.inflate(R.layout.students_item, this, true);
+
+            name = (TextView) findViewById(R.id.student_item_name);
+            number = (TextView) findViewById(R.id.student_item_number);
+        }
+
+        public void setTitle(String name) {
+            this.name.setText(name);
+        }
+
+        public void setInfo(String number) {
+            this.number.setText(number);
+        }
+    }
+
+    class ListViewAdapter extends BaseAdapter {
+
+        ArrayList<UserInfo> items = new ArrayList<>();
+
+        public ListViewAdapter(CreateLec_2_Activity createLec_2_activity) {
+
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public UserInfo getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            CreateLec_2_Activity.StudentNumberItemView itemView = new CreateLec_2_Activity.StudentNumberItemView(getApplicationContext());
+
+            UserInfo item = items.get(position);
+            itemView.setTitle(item.getName());
+            String number = item.getStunum();
+            itemView.setInfo(number);
+            return itemView;
+        }
+
+        public void addItem(UserInfo item) {
+            items.add(item);
+        }
+
+        public void clear() {
+            items = new ArrayList<UserInfo>();
+        }
     }
 }
