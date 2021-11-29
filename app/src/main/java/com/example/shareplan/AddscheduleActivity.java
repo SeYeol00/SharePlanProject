ea12389b1b62e764291a21ece6c9c535b110b683
choieunsol <achldmsthf@kookmin.ac.kr>
2021-11-29 21:47:02 +0900

    코드 설명 주석

--- app/src/main/java/com/example/shareplan/AddscheduleActivity.java
+++ app/src/main/java/com/example/shareplan/AddscheduleActivity.java
@@ -36,9 +36,13 @@ public class AddscheduleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_schedule);
-        editText = findViewById(R.id.schedule_name);
+
         radioGroup = findViewById(R.id.radiogroup);
+        editText = findViewById(R.id.schedule_name);
+        datePicker = findViewById(R.id.date);
+
         TodoInfo todoInfo = new TodoInfo();
+
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
@@ -61,15 +65,16 @@ public class AddscheduleActivity extends AppCompatActivity {
             }
         });
 
-        // 선택된 날짜로 date picker 초기화
-        datePicker = findViewById(R.id.date);
+        // 달력에서 선택된 날짜 intent로 받아오기
         Intent intent = getIntent();
         int year = intent.getIntExtra("year", 0);
         int month = intent.getIntExtra("month", 0);
         int day = intent.getIntExtra("day", 0);
-        // date picker 건들이지 않아도 todoInfo 객체에 선택된 날짜 속성 넣어주기
+        // date picker 건들이지 않아도 todoInfo 객체에 date 속성 set
         todoInfo.setDate(String.valueOf(year) + "-" + String.valueOf(month+1) + "-" + String.valueOf(day));
+        // datePicker intent로 받아온 날짜 데이터로 초기화
         datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            // datePicker에 변화 생기면 todoInfo date 속성 set
             @Override
             public void onDateChanged(DatePicker datePicker, int i, int i1, int i2) {
                 String year = String.valueOf(i);
@@ -86,9 +91,11 @@ public class AddscheduleActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String todoname = editText.getText().toString();
-                todoInfo.setTitle(todoname);
-                if(!todoInfo.getTitle().equals("")&&!todoInfo.getType().equals("")&&!todoInfo.getDate().equals("")){
+                String title = editText.getText().toString();
+                todoInfo.setTitle(title);
+
+                // 일정 정보를 모두 입력하였을 경우
+                if(!todoInfo.getTitle().equals("") && !todoInfo.getType().equals("") && !todoInfo.getDate().equals("")){
                     databaseReference.child("TodoInfo").child(lec_uid).child(todoInfo.getDate()).addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -106,8 +113,8 @@ public class AddscheduleActivity extends AppCompatActivity {
 
                         }
                     });
-                } else {
-                    Toast.makeText(getApplicationContext(), "빈칸을 입력해 주세요", Toast.LENGTH_LONG).show();
+                } else {    // 입력하지 않은 일정 정보가 있을 경우
+                    Toast.makeText(getApplicationContext(), "모든 정보를 입력해 주세요", Toast.LENGTH_LONG).show();
                 }
 
             }
